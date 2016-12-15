45ac97bf66f840531cf5e846fa51663997fcfe22
Roxana Jaramillo <roxandreajc110@hotmail.com>
2016-12-14 21:48:39 -0500

    Creating form restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,69 @@
 class RestaurantsController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def delete
-  end
-
-  def show
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    find_restaurant
+    if @restaurant.nil?
+      flash[:error] = "Houston we are in troubles, please dont hack us."
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user_id = current_client.id
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      flash[:success] = "Restaurant created. Ok"
+      render 'new'
+    end
+  end
+
+  def destroy
+    find_restaurant
+    if @restaurant.destroy
+      flash[:success] = "Restaurant Destroyed Successfully"
+    else
+      flash[:error] = "Houston we are in troubles, please try it later"
+    end
+    redirect_to restaurants_path
+  end
+
+  def update
+    find_restaurant
+    if @restaurant.update(restaurant_params)
+      flash[:success] = "Restaurant updated. Ok"
+      redirect_to restaurants_path
+    else
+      flash[:error] = "Houston we are in troubles, try it again."
+      render 'edit'
+    end
+  end
+
+
+  def show
+    find_restaurant
+    if @restaurant.nil?
+      flash[:error] = "Houston we are in troubles, please dont hack us."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  #strong parameters
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :gps, :image)
   end
 end
